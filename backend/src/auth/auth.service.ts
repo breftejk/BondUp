b2415fe722b2ae549f01d874694b6ea9ed8b3a11
Marcin Kondrat <breftejk@gmail.com>
2025-08-18 17:03:15 +0200

    feat(auth.service.ts): replace axios with jwks-rsa for token validation to enhance security and performance chore(package.json): remove axios dependency and add jwks-rsa for improved JWT handling

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
-import axios from 'axios';
-import * as jwt from 'jsonwebtoken';
 import { config } from '@/config';
+import jwksRsa from 'jwks-rsa';
+import jwt, { JwtHeader, SigningKeyCallback } from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
@@ -13,10 +13,10 @@ export class AuthService {
 
     if (provider === 'google') {
       email = await this.verifyGoogleToken(idToken);
-    }
-
-    if (provider === 'apple') {
+    } else if (provider === 'apple') {
       email = await this.verifyAppleToken(idToken);
+    } else {
+      throw new UnauthorizedException('Unsupported provider');
     }
 
     if (!email) {
@@ -32,27 +32,63 @@ export class AuthService {
     return { userId: user.id };
   }
 
+  /** Google Sign In token validation */
   private async verifyGoogleToken(
     idToken: string,
   ): Promise<string | undefined> {
-    const { data } = await axios.get(
-      `https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`,
-    );
+    const googleClient = jwksRsa({
+      jwksUri: 'https://www.googleapis.com/oauth2/v3/certs',
+      cache: true,
+      cacheMaxEntries: 5,
+      cacheMaxAge: 3600_000,
+    });
 
-    if (data.aud !== config.GOOGLE_CLIENT_ID) {
+    const getKey = (header: JwtHeader, cb: SigningKeyCallback) => {
+      googleClient.getSigningKey(header.kid!, (err, key) => {
+        const publicKey = key?.getPublicKey();
+        cb(err, publicKey);
+      });
+    };
+
+    try {
+      const decoded = jwt.verify(idToken, getKey, {
+        algorithms: ['RS256'],
+        issuer: ['https://accounts.google.com', 'accounts.google.com'],
+        audience: config.GOOGLE_CLIENT_ID,
+      }) as any;
+
+      return decoded.email;
+    } catch {
       return undefined;
     }
-
-    return data.email;
   }
 
+  /** Apple Sign In token validation */
   private async verifyAppleToken(idToken: string): Promise<string | undefined> {
-    const decoded = jwt.decode(idToken) as any;
+    const appleClient = jwksRsa({
+      jwksUri: 'https://appleid.apple.com/auth/keys',
+      cache: true,
+      cacheMaxEntries: 5,
+      cacheMaxAge: 3600_000,
+    });
 
-    if (decoded?.aud !== config.APPLE_SIWA_CLIENT_ID) {
+    const getKey = (header: JwtHeader, cb: SigningKeyCallback) => {
+      appleClient.getSigningKey(header.kid!, (err, key) => {
+        const publicKey = key?.getPublicKey();
+        cb(err, publicKey);
+      });
+    };
+
+    try {
+      const decoded = jwt.verify(idToken, getKey, {
+        algorithms: ['RS256'],
+        issuer: 'https://appleid.apple.com',
+        audience: config.APPLE_SIWA_CLIENT_ID,
+      }) as any;
+
+      return decoded.email;
+    } catch {
       return undefined;
     }
-
-    return decoded?.email;
   }
 }
